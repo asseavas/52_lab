dae780e90ec1dd1853da9970daad7635350a0bc6
Klimova Alina <alinaklimova82@gmail.com>
2024-05-22 01:24:53 +0600

    Lvl 1 done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import './App.css'
+import './App.css';
+import Card from "./components/Card/Card.tsx";
 
 const App = () => {
   return (
-    <>
-
-    </>
-  )
+    <div className="playingCards faceImages">
+      <Card rank={"9"} suit={"diams"}/>
+    </div>
+  );
 };
 
-export default App
+export default App;
---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+interface Props {
+  rank: string;
+  suit: string;
+}
+
+const suitSymbols: { [key: string]: string } = {
+  diams: "♦",
+  hearts: "♥",
+  clubs: "♣",
+  spades: "♠",
+};
+
+const Card: React.FC<Props>= ({rank, suit}) => {
+    const suitSymbol = suitSymbols[suit];
+    return (
+        <span className={`card rank-${rank.toLowerCase()} ${suit}`}>
+            <span className="rank">{rank}</span>
+            <span className="suit">{suitSymbol}</span>
+        </span>
+    );
+};
+
+export default Card;
